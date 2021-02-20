c300dbed63d7943c242408cde788f023319e6c78
Orlando <baello2020@gmail.com>
2021-02-19 16:41:42 -0800

    server.js updated

--- server.js
+++ server.js
@@ -1,46 +1,40 @@
 // Orlando Baello - Homework 17
-// Fitness Tracker
-
+// REQUIRE our necessary packages.
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// Require our model
+require("./models");
 
+// Declare our PORT and app.
 const PORT = process.env.PORT || 8080;
-
-const db = require("./models");
-
 
 const app = express();
 
-const databaseName = "orlando"
-
+// Set our middlewares for our app.
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-//Mongodb
-
+// Connect to Mongoose.
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/orlando',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+  process.env.MONGODB_URI || "mongodb://localhost/orlando",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
+// REQUIRE our HTML and API routes.
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes.js")(app);
 
-// Requiring our routes
-
-app.use("/api", require("./routes/api-routes.js"));
-app.use("/", require("./routes/html-routes.js"));
-
-
+// Listen to our server.
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}`);
 });
